08d9f59eed0b4ee4bee019793364deb265b7b590
Jordan Parker <parkerj4321@gmail.com>
2018-07-31 15:12:24 -0500

    Exercises 20 and 21

---
+++ Exercise20.js
@@ -0,0 +1,8 @@
+function main(){
+    var counter = -10;
+    while(counter< 5){
+        console.log(counter);
+        counter ++;
+    }
+}
+main();
---
+++ Exercise21.js
@@ -0,0 +1,9 @@
+function main(){
+
+    var counter = 0;
+    while(counter<101){
+        console.log(counter);
+        counter += 2;
+    }
+}
+main();
